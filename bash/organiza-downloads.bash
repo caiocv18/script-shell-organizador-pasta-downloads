5b0b21e14d7a5d05547706109f2d1d80314771df
Caio Vinicius <caioviniciuscv18@gmail.com>
2023-09-29 23:38:41 -0300

    Passo 1 - Informar que o script foi iniciado

---
+++ bash/organiza-downloads.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Mensagem informando que o script está sendo iniciado
+echo "Iniciando o script de organização da pasta de downloads..."
---
+++ powershell/organiza-downloads.ps1
@@ -0,0 +1,6 @@
+﻿# Codificando com UTF-8 para que seja possível a utilização de acentuações
+$script = Get-Content -Path organiza-downloads.ps1
+$script | Set-Content -Path organiza-downloads.ps1 -Encoding UTF8
+
+# Mensagem informando que o script está sendo iniciado
+Write-Host "Iniciando o script de organização da pasta de downloads..."
